60eb24bd69884292c867f3b31f27a3d711aab5a2
Willis Aybar <willisaybar@gmail.com>
2020-02-01 14:05:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,16 @@
-def oxford_comma(array)
-   array = []
+# def oxford_comma(array)
+#   array = []
     
+# end
+
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
